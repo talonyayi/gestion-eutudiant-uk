af118dd3717f4609c861fb95e76f03aed56f1eca
talonyayi <talonyayi@gmail.com>
2026-06-05 04:08:10 +0200

    Ajout du code dans Student

---
+++ Student.java
@@ -0,0 +1,9 @@
+public class Student {
+private int id;
+private String nom;
+private double note;
+public Student(int id, String nom, double note) {
+this.id = id;
+this.nom = nom;this.note = note;
+}
+}
